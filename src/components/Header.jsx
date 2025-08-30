707d56943f10126642966a35a020851699ab960e
baizid20 <baizidul20@gmail.com>
2025-08-30 22:29:42 +0600

    header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='pb-25'>
+            <h1 className='text-5xl font-bold  text-center font-mono'> <span className='text-[#FF9A00]'>Kilo</span><span className='text-[#4F200D]'>Mail</span></h1>
+        </div>
+    );
+};
+
+export default Header;
